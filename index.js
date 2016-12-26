8dc76e1f96eff1a13a0586101c76ae0c9f143a6b
Rawnly <fedevitale99@gmail.com>
2016-12-26 19:04:08 +0100

    FIX

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
-const https = require('https');
-const path = require('path');
-const random = require("randomstring");
+const fs = require('fs')
+const https = require('https')
+const path = require('path')
+const random = require('randomstring')
 
 module.exports = (file_name, url, destination, callback) => {
-	'use strict';
+	'use strict'
 	if ( !file_name ) {
 		file_name = random.generate({
 			length: 12,
 			charset: 'readable',
 			capitalization: 'lowercase'
-		}) + path.extname(url);
+		}) + path.extname(url)
 	}
 
 	if ( !callback ) {
@@ -19,13 +19,12 @@ module.exports = (file_name, url, destination, callback) => {
 		}
 	}
 
-	var join = path.join;
-	var file = fs.createWriteStream( join(destination, file_name) );
+	var join = path.join
+	var file = fs.createWriteStream( join(destination, file_name) )
 
-	spin.start();
 	https.get(url, function(response) {
 		response.pipe(file).on('finish', () => {
-			callback();
-		});
-	});
+			callback()
+		})
+	})
 }
